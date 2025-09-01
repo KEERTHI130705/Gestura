713e57f398d15e270b53647b82b12ce6b57b8447
KEERTHI130705 <alle.100523735001@gmail.com>
2025-09-01 14:39:27 +0530

    Added all the letters

--- test.py
+++ test.py
@@ -1,35 +1,45 @@
+
 import cv2
 import numpy as np
 import tensorflow as tf
 from cvzone.HandTrackingModule import HandDetector
 
+
 # Load the TFLite model
 interpreter = tf.lite.Interpreter(model_path="Model/model_unquant.tflite")
 interpreter.allocate_tensors()
 
+
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
+
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
 
+
 imgSize = 300
 offset = 20
 
+
 # Labels
-labels = ["A", "B", "C"]
+labels = ["A", "B", "C","D", "E", "F", "G", "I", "K", "L", "M", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y","Z","H","N"]
+
 
 while True:
     success, img = cap.read()
     imgOutput = img.copy()
     hands, img = detector.findHands(img)
 
+
     if hands:
         hand = hands[0]
         x, y, w, h = hand['bbox']
 
+
         imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
         imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
+
 
         try:
             aspectRatio = h / w
@@ -46,9 +56,11 @@ while True:
                 hGap = (imgSize - hCal) // 2
                 imgWhite[hGap:hGap + hCal, :] = imgResize
 
+
             # Preprocess for model
             imgInput = cv2.resize(imgWhite, (224, 224))   # Teachable Machine usually exports 224x224
             imgInput = np.expand_dims(imgInput, axis=0).astype(np.float32) / 255.0
+
 
             # Run inference
             interpreter.set_tensor(input_details[0]['index'], imgInput)
@@ -57,14 +69,18 @@ while True:
             index = np.argmax(output_data)
             prediction = output_data[0]
 
+
             # Show label
-            cv2.putText(imgOutput, labels[index], (x, y - 26), 
+            cv2.putText(imgOutput, labels[index], (x, y - 26),
                         cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 0, 255), 2)
+
 
             print("Prediction:", prediction, "Class:", labels[index])
 
+
         except Exception as e:
             print("Error:", e)
+
 
     cv2.imshow("Image", imgOutput)
     cv2.waitKey(1)
